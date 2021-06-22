9554e2c6463e0c3a039ec2b2947399d1a7a95bab
erwan.vivien <erwan.vivien@epita.fr>
2021-06-22 20:10:56 +0200

    split test in other file

---
+++ tests.py
@@ -0,0 +1,260 @@
+import unittest
+from tables import ACTable, DCTable
+from compress import DCT_coeffs_8x8, quantization, huffman_AC_encoding, \
+    huffman_DC_encoding, zigzag, huffman_number
+
+
+class TestJPEG(unittest.TestCase):
+    def test_dct_1(self):
+        input_pixel_matrix = [140, 144, 147, 140, 140, 155, 179, 175,
+                              144, 152, 140, 147, 140, 148, 167, 179,
+                              152, 155, 136, 167, 163, 162, 152, 172,
+                              168, 145, 156, 160, 152, 155, 136, 160,
+                              162, 148, 156, 148, 140, 136, 147, 162,
+                              147, 167, 140, 155, 155, 140, 136, 162,
+                              136, 156, 123, 167, 162, 144, 140, 147,
+                              148, 155, 136, 155, 152, 147, 147, 136]
+
+        output_pixel_matrix_expected = [186, -18, 15, -9, 23, -9, -14, -19,
+                                        21, -34, 26, -9, -11, 11, 14, 7,
+                                        -10, -24, -2, 6, -18, 3, -20, -1,
+                                        -8, -5, 14, -15, -8, -3, -3, 8,
+                                        -3, 10, 8, 1, -11, 18, 18, 15,
+                                        4, -2, -18, 8, 8, -4, 1, -7,
+                                        9, 1, -3, 4, -1, -7, -1, -2,
+                                        0, -8, -2, 2, 1, 4, -6, 0]
+
+        output_pixel_matrix = DCT_coeffs_8x8(input_pixel_matrix)
+
+        self.assertListEqual(output_pixel_matrix, output_pixel_matrix_expected)
+        # for i in range(64):
+        #     if output_pixel_matrix[i] != output_pixel_matrix_expected[i]:
+        #         print("test_dct_1", i,
+        #               output_pixel_matrix[i], output_pixel_matrix_expected[i])
+
+    def test_division_wise_1(self):
+        input_dct = [
+            -415, -30, -61, 27, 56, -20, -2, 0,
+            4, -22, -61, 10, 13, -7, -9, 5,
+            -47, 7, 77, -25, -29, 10, 5, -6,
+            -49, 12, 34, -15, -10, 6, 2, 2,
+            12, -7, -13, -4, -2, 2, -3, 3,
+            -8, 3, 2, -6, -2, 1, 4, 2,
+            -1, 0, 0, -2, -1, -3, 4, -1,
+            0, 0, -1, -4, -1, 0, 1, 2]
+
+        output_expected = [
+            -26, -3, -6, 2, 2, -1, 0, 0,
+            0, -2, -4, 1, 1, 0, 0, 0,
+            -3, 1, 5, -1, -1, 0, 0, 0,
+            -3, 1, 2, -1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ]
+
+        output = quantization(input_dct)
+        output = [int(e) for e in output]
+        print("BUG IN `test_division_wise_1`")
+        # self.assertListEqual(output, output_expected, msg="Test failed")
+
+    def test_zigzag(self):
+        input_qtz = [
+            -26, -3, -6, 2, 2, -1, 0, 0,
+            0, -2, -4, 1, 1, 0, 0, 0,
+            -3, 1, 5, -1, -1, 0, 0, 0,
+            -3, 1, 2, -1, 0, 0, 0, 0,
+            1, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ]
+
+        output_expected = [
+            -26, -3, 0, -3, -2, -6, 2,
+            -4, 1, -3, 1, 1, 5, 1, 2,
+            -1, 1, -1, 2, 0, 0, 0, 0,
+            0, -1, -1
+        ]
+
+        self.assertListEqual(zigzag(input_qtz), output_expected)
+
+    def test_huffman_number(self):
+        self.assertEqual(huffman_number(-5), 2)
+        self.assertEqual(huffman_number(-3), 0)
+        self.assertEqual(huffman_number(-2), 1)
+        self.assertEqual(huffman_number(2), 2)
+        self.assertEqual(huffman_number(3), 3)
+        self.assertEqual(huffman_number(-1024), 1023)
+        self.assertEqual(huffman_number(1024), 1024)
+        self.assertEqual(huffman_number(-35), 28)
+
+    def test_huffman_encoding_DC(self):
+        self.assertEqual(huffman_DC_encoding(-3), 16)
+        self.assertEqual(huffman_DC_encoding(3), 19)
+        self.assertEqual(huffman_DC_encoding(-1), 6)
+        self.assertEqual(huffman_DC_encoding(1), 7)
+
+    def test_huffman_encoding_AC(self):
+        self.assertEqual(huffman_AC_encoding(3, 0), 7)
+        for i in range(0, 14):
+            self.assertEqual(huffman_AC_encoding(-3, i),
+                             ACTable[i][2]["basecode"] << ACTable[i][2]["shift"])
+        self.assertEqual(huffman_AC_encoding(-2, 0), 0b0101)
+        self.assertEqual(huffman_AC_encoding(-6, 0), 0b100001)
+        self.assertEqual(huffman_AC_encoding(2, 0), 0b0110)
+        self.assertEqual(huffman_AC_encoding(-4, 0), 0b100011)
+        self.assertEqual(huffman_AC_encoding(-1, 5), 0b11110100)
+        self.assertEqual(huffman_AC_encoding(-1, 0), 0b000)
+
+        # for seed in range(10000):
+        #     random.seed(a=seed)
+        #     for i, j in itertools.product(range(1, 11), range(1, 14)):
+        #         val = random.randrange(0, 2 ** (i - 1))
+        #         self.assertEqual(huffman_AC_encoding(-(2 ** i) + 1 + val, j),
+        #                          ACTable[j][i]["basecode"] << ACTable[j][i]["shift"] | val)
+
+        self.assertEqual(huffman_AC_encoding(-1, 1), 0b11000)
+        self.assertEqual(huffman_AC_encoding(-1, 2), 0b110110)
+        self.assertEqual(huffman_AC_encoding(-1, 3), 0b1110100)
+        self.assertEqual(huffman_AC_encoding(-1, 4), 0b1110110)
+        self.assertEqual(huffman_AC_encoding(-1, 5), 0b11110100)
+        self.assertEqual(huffman_AC_encoding(-1, 6), 0b11110110)
+        self.assertEqual(huffman_AC_encoding(-1, 7), 0b111110010)
+        self.assertEqual(huffman_AC_encoding(-1, 8), 0b111110100)
+        self.assertEqual(huffman_AC_encoding(-1, 9), 0b1111110000)
+        self.assertEqual(huffman_AC_encoding(-1, 10), 0b1111110010)
+        self.assertEqual(huffman_AC_encoding(-1, 11), 0b1111110100)
+        self.assertEqual(huffman_AC_encoding(-1, 12), 0b11111110100)
+        self.assertEqual(huffman_AC_encoding(-1, 13), 0b111111110100)
+
+        self.assertEqual(huffman_AC_encoding(-3, 1), 0b11100100)
+        self.assertEqual(huffman_AC_encoding(-3, 2), 0b1111100000)
+        self.assertEqual(huffman_AC_encoding(-3, 3), 0b11111011100)
+        self.assertEqual(huffman_AC_encoding(-3, 4), 0b111111100000)
+        self.assertEqual(huffman_AC_encoding(-3, 5), 0b111111100100)
+        self.assertEqual(huffman_AC_encoding(-3, 6), 0b1111111100000)
+        self.assertEqual(huffman_AC_encoding(-3, 7), 0b1111111100100)
+        self.assertEqual(huffman_AC_encoding(-3, 8), 0b11111111100000000)
+        self.assertEqual(huffman_AC_encoding(-3, 9), 0b111111111011111100)
+        self.assertEqual(huffman_AC_encoding(-3, 10), 0b111111111100100000)
+        self.assertEqual(huffman_AC_encoding(-3, 11), 0b111111111101000100)
+        self.assertEqual(huffman_AC_encoding(-3, 12), 0b111111111101101000)
+        self.assertEqual(huffman_AC_encoding(-3, 13), 0b111111111110001100)
+
+        # self.assertEqual(huffman_AC_encoding(-7, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-7, 13), 0b   )
+
+        # self.assertEqual(huffman_AC_encoding(-15, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-15, 13), 0b   )
+
+        # self.assertEqual(huffman_AC_encoding(-31, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-31, 13), 0b   )
+
+        # self.assertEqual(huffman_AC_encoding(-63, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-63, 13), 0b   )
+
+        # self.assertEqual(huffman_AC_encoding(-127, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-127, 13), 0b   )
+
+        # self.assertEqual(huffman_AC_encoding(-255, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-255, 13), 0b   )
+
+        # self.assertEqual(huffman_AC_encoding(-511, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-511, 13), 0b   )
+
+        # self.assertEqual(huffman_AC_encoding(-1023, 1), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 2), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 3), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 4), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 5), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 6), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 7), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 8), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 9), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 10), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 11), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 12), 0b   )
+        # self.assertEqual(huffman_AC_encoding(-1023, 13), 0b   )
+
+
+if __name__ == "__main__":
+    unittest.main()
